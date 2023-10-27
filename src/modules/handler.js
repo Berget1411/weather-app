d1a3b29494d9f94fe848c94e78ca5ccee9f23816
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2023-10-27 20:25:26 +0200

    user can switch between metric and imperial unit

--- src/modules/handler.js
+++ src/modules/handler.js
@@ -5,14 +5,20 @@ import '../styles/main.scss';
 const handler = () => {
   const searchForm = document.querySelector('#search-form');
   const searchInput = document.querySelector('#search-input');
-  const unit = 'imperial';
+  const useMetricButton = document.querySelector('#metric');
+  const useImperialButton = document.querySelector('#imperial');
+  let unit = 'metric';
+  let location = 'Sweden';
 
-  searchForm.addEventListener('submit', searchWeather);
+  searchForm.addEventListener('submit', submitForm);
 
-  function searchWeather(e) {
+  function submitForm(e) {
     e.preventDefault();
-    const location = searchInput.value;
+    location = searchInput.value;
+    displayWeather(location, unit);
+  }
 
+  function displayWeather(location, unit) {
     api
       .getCurrentWeather(location, unit)
       .then((res) => {
@@ -30,6 +36,19 @@ const handler = () => {
       .catch((rej) => {
         dom.displayError(rej);
       });
+  }
+
+  useMetricButton.addEventListener('click', changeUnit);
+  useImperialButton.addEventListener('click', changeUnit);
+
+  function changeUnit(e) {
+    unit = e.target.id;
+
+    document.querySelectorAll('.units button').forEach((button) => {
+      button.classList.remove('active-unit');
+    });
+    e.target.classList.toggle('active-unit');
+    displayWeather(location, unit);
   }
 };
 
